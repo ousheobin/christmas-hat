22969b5e010b210a474d1febca00682a6465fa79
Github Action Robort <ousheobin@users.noreply.github.com>
2020-12-24 14:51:10 +0000

    [Automation] Update github page.

---
+++ precache-manifest.14d35de359bf849604bf98ace7c19689.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "654c4fd9ae037099040d",
+    "url": "/christmas/css/app.71c8f0d1.css"
+  },
+  {
+    "revision": "e9700f8700b53d833651420100415cdc",
+    "url": "/christmas/img/common/cat.jpg"
+  },
+  {
+    "revision": "972bd083d80e7a3647733ed2e6cb0137",
+    "url": "/christmas/img/common/hat.png"
+  },
+  {
+    "revision": "a14f0ef629784bd5350e6e354b831db7",
+    "url": "/christmas/index.html"
+  },
+  {
+    "revision": "654c4fd9ae037099040d",
+    "url": "/christmas/js/app.a89915cc.js"
+  },
+  {
+    "revision": "77a00717403b7579f43f",
+    "url": "/christmas/js/chunk-vendors.ea89561e.js"
+  },
+  {
+    "revision": "aa0e8957eee9dd39b74b181a5b3fec87",
+    "url": "/christmas/manifest.json"
+  },
+  {
+    "revision": "be5f8eb010a04612c604ec4cda173051",
+    "url": "/christmas/model/group1-shard1of1.bin"
+  },
+  {
+    "revision": "b6ab822ba1f75d4ccafb0e189c5cfd27",
+    "url": "/christmas/model/model.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/christmas/robots.txt"
+  }
+]);
